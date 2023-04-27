cfa722ab28859dea59b653d5e1d0870138540f52
kirrez <kuharevkirill@gmail.com>
2023-04-27 12:25:43 +0300

    Parallax3D added
    Updating texture's offset instead of animation. Shader - Mobile diffuse

---
+++ Assets/PlatformerGame/Scripts/Parallax3D.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer
+{
+    public class Parallax3D : MonoBehaviour
+    {
+        [SerializeField]
+        private float ScrollSpeed = 5f;
+
+        [SerializeField]
+        private Renderer Renderer;
+
+        private void Update()
+        {
+            float offset = Time.time * ScrollSpeed;
+            Renderer.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+
+        }
+    }
+}
